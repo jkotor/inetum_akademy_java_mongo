a49b53994b1f22f991d0a31549d8792177b55c1c
jkotor <jakub.kotorowicz@gmail.com>
2021-12-14 15:29:17 +0100

    Zadania, part 1

--- src/test/java/com/inetum/academy/embedded/InMemoryMongoTest.java
+++ src/test/java/com/inetum/academy/embedded/InMemoryMongoTest.java
@@ -37,4 +37,50 @@ public class InMemoryMongoTest {
         System.out.println();
     }
 
+    @Test
+    void createDatabaseAndCollection() {
+        // utworzyć baze danych "akademia" a w środku 2 kolekcje 'users' i 'addresses', wyświetlić wszystkie kolekcje
+        //    MongoClient.getDatabase(), MongoDatabase.createCollection(), MongoDatabase.listCollectionNames()
+
+    }
+
+    @Test
+    void insertSampleDocuments() {
+        // wstawić do kolekcji "users" użytkownika - Dokument z dwoma polami "name" i "age" oraz polem "_id"=1
+        // wyświetlić zawartość kolekcji
+        // wstawić kolejnego użytkownika z "_id"=2 i dowolnymi "name" i "age"
+        // wyświetlić zawartość kolekcji
+        //    Document(), Document.append(), MongoCollection.insertOne/Many(), MongoCollection.find()
+        // [opcjonalnie] zrobić odpowiednie asercje
+
+    }
+
+    @Test
+    void shouldThrowWhenDuplicatingID() {
+        // wstawić do kolekcji "users" 2 użytkowników z tym samym "_id" i dowolnymi innymi polami
+        // obsłużyć asercję na zgłaszany wyjątek, np używając org.junit.jupiter.api.Assertions.assertThrows()
+
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    void shouldGenerateIdsDuringInsertion() {
+        // wstawić do kolekcji "users" 2 użytkowników z dowolnymi polami "name", "age", ale bez "_id"
+        // wyświetlić zawartość kolekcji (co się stało?)
+        // dorobić asercje na zaobserwowaną sytuację
+
+        // given
+
+        // when
+
+        // then
+
+    }
+
+
 }
